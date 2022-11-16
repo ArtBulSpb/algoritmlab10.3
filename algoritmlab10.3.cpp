75b38df9fe88fd8e7307ec9f0b978ed776ce501e
ArtBulSpb <arturtomsk2005@gmail.com>
2022-11-16 22:20:06 +0300

    Добавьте файлы проекта.

---
+++ algoritmlab10.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+int num, sum = 0, a;
+
+
+using namespace std;
+
+int summator(int number) {
+    if (num == 0) {
+        return sum;
+    }
+    a = num % 10;
+    num = (num - a) / 10;
+    sum = sum + a;
+    return summator(a);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите натуральное число ";
+    cin >> num;
+    cout << summator(num) << endl;
+    return 0;
+}
